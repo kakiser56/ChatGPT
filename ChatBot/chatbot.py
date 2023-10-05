6dc889c53a6a39454803036fa8d31370c2771704
Keith Kiser <kakiser56@me.com>
2023-10-05 14:07:15 -0400

    add on to chatbot

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,4 +1,23 @@
 import openai
 from dotenv import dotenv_values
 
-env = dotenv_values('.env')
+config = dotenv_values('.env')
+
+openai.api_key = config['OPENAI_API_KEY']
+
+
+while True:
+    messages = []
+    try:
+        user_input = input(("You: "))
+        messages.append({"role":"user","content": user_input})
+
+        res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages = messages
+        )
+        print('Chat GPT: ' + res["choices"][0]["message"]["content"])
+    except KeyboardInterrupt:
+        print('Exiting')
+        break;
+
